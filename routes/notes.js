dd7a62def5c3e122367b92381014d8163fffe572
Alex Arruda <alexrodrigoarruda@gmail.com>
2022-10-18 21:47:54 -0300

    Feita a rota que insere as notas criadas pelo usuário no banco

--- routes/notes.js
+++ routes/notes.js
@@ -1,3 +1,7 @@
+const db = require("../db/connection");
+
+const { ObjectId } = require("mongodb");
+
 const Router = require("express").Router;
 
 const router = Router();
@@ -5,6 +9,19 @@ const router = Router();
 router.get("/", (req, res) => {
 
     res.render("notes/create");
+
+});
+
+router.post("/", (req, res) => {
+
+    const { title, description }= req.body;
+
+    db.getdb()
+    .db()
+    .collection("notes")
+    .insertOne({ title, description });
+
+    res.redirect(301, "/");
 
 })
 
